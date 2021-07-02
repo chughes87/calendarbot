6842b088bfde21c15ae238156e4f564e19e124f0
Charles Hughes <chughes87@gmail.com>
2021-07-02 00:08:44 -0700

    be clever

--- src/getSecret.js
+++ src/getSecret.js
@@ -1,5 +1,6 @@
-const { bind, pipe, prop } = require('ramda');
-const { promisify } = require('util')
+const { pair, apply, objOf, bind, pipe, prop } = require('ramda');
+const { promisify } = require('util');
+const { pipeP } = require('./ramdaP');
 
 var AWS = require('aws-sdk'),
   region = 'us-east-1',
@@ -12,10 +13,11 @@ var client = new AWS.SecretsManager({
 
 const getSecret = promisify(bind(client.getSecretValue, client));
 
-module.exports = (secretName) =>
-  getSecret({ SecretId: secretName })
-    .then(pipe(
-      prop('SecretString'),
-      JSON.parse
-    ))
+module.exports = pipeP([
+  pair('SecretId'),
+  apply(objOf),
+  getSecret,
+  prop('SecretString'),
+  JSON.parse
+]);
 
