e4439e903ff6f6f4d4b8568e0eeeae9578292ac0
Charles Hughes <chughes87@gmail.com>
2021-07-02 14:55:01 -0700

    fix timezone

--- src/index.js
+++ src/index.js
@@ -8,18 +8,19 @@ const sendSMS = require('./sendSMS');
 const now = new Date();
 const newDate = x => new Date(x);
 const isNow = date =>
-  date.getHours() === now.getHours() &&
-  date.getDate() === now.getDate();
+  date.getUTCHours() === (now.getUTCHours() + 1) &&
+  date.getUTCDate() === now.getUTCDate();
 
-module.exports.handler = pipeP([
-  getSecret,
-  (auth) =>
-    pipeP([
-      getEvents,
-      path(['data', 'items']),
-      filter(pipe(path(['start', 'dateTime']), newDate, isNow)),
-      processEvents,
-      sendSMS(auth),
-    ])(auth)
-])('calendarbot_auth');
+module.exports.handler = () =>
+  pipeP([
+    getSecret,
+    (auth) =>
+      pipeP([
+        getEvents,
+        path(['data', 'items']),
+        filter(pipe(path(['start', 'dateTime']), newDate, isNow)),
+        processEvents,
+        sendSMS(auth),
+      ])(auth)
+  ])('calendarbot_auth');
 
